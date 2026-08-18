c9a4182428f3f0c568b574751bed9eaa637c2fb7
i-bro <ibrogp2@gmail.com>
2026-08-18 16:52:45 +0200

    homework 1 done

---
+++ homework 01/UsersApiApp/UsersApiApp/Controllers/UsersController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UsersApiApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<string>> GetUsers()
+        {
+            return Ok(StaticDb.Users);
+        }
+
+        [HttpGet("{index}")]
+        public ActionResult<string> GetByIndex(int index)
+        {
+            try
+            {
+                if(index < 0)
+                {
+                    return BadRequest("The index cannot be negative value");
+                }
+                if(index >= StaticDb.Users.Count)
+                {
+                    return StatusCode(StatusCodes.Status404NotFound, $"User with index {index} does not exist");
+                }
+
+                return Ok(StaticDb.Users[index]);
+            }
+            catch(Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "An error occured please try later");
+            }
+        }
+    }
+}
---
+++ homework 01/UsersApiApp/UsersApiApp/StaticDb.cs
@@ -0,0 +1,14 @@
+﻿namespace UsersApiApp
+{
+    public static class StaticDb
+    {
+        public static List<string> Users = new List<string>
+        {
+            "Bob Bobski",
+            "Petko Petkovski",
+            "Mile Panika",
+            "Toso Malerot",
+            "Cacko Konopiski"
+        };
+    }
+}
